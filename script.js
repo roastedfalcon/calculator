549191c19b81836355ab04919f9ed087a437c37d
Mark Schlining <maschlining@yahoo.com>
2022-08-02 12:34:34 -0400

    First Working Draft

--- script.js
+++ script.js
@@ -1,21 +1,86 @@
-function operate(a, b, operator) {
-    operator(a, b);
-}
+
 
 const display = document.querySelector('#display');
 const nums = document.querySelectorAll('.number');
-nums.forEach(num => num.addEventListener('click', () => {
-    if (display.textContent == '0') display.textContent = '';
-    display.textContent += num.textContent;
-}))
-
 const clear = document.querySelector('#clear');
+const del = document.querySelector('#delete');
+const decimal = document.querySelector('#decimal');
+const operators = document.querySelectorAll('.operator');
+const eval = document.querySelector('#equals');
+console.log(operators);
 clear.addEventListener('click', () => window.location.reload());
 
-const del = document.querySelector('#delete');
 del.addEventListener('click', () => {
     display.textContent = display.textContent.slice(0, -1);
     if (display.textContent == '') display.textContent = '0';
 });
 
-const decimal = document.querySelector('#decimal');
+
+
+
+nums.forEach(num => num.addEventListener('click', getFirstNum));
+
+operators.forEach(operator => operator.addEventListener('click', opEvent));
+
+function opEvent(e) {
+    if (display.textContent !== '0') {
+        this.classList.toggle('darker');
+        if (this.classList.contains('darker')) {
+            unselectOthers(e);
+            globalThis.firstNum = Number(display.textContent);
+            nums.forEach(num => {
+                num.removeEventListener('click', getFirstNum);
+                num.addEventListener('click', getSecondNumber);
+            });
+            console.log('meow');
+}}
+}
+
+
+//remove darker class from operator when another operator is selected
+function unselectOthers(e) { 
+    let selected = e.srcElement.innerText;
+    operators.forEach(operator => {
+        if (selected !== operator.innerText) operator.classList.remove('darker');
+    });
+}
+
+
+function getFirstNum() {
+    if (display.textContent == '0') display.textContent = '';
+    display.textContent += this.textContent;
+}
+
+
+function getSecondNumber() {
+    display.textContent = this.textContent;
+    operators.forEach(operator => operator.removeEventListener('click', opEvent));
+    nums.forEach(num => {
+        num.removeEventListener('click', getSecondNumber);
+        num.addEventListener('click', () => display.textContent += num.textContent);
+    });
+    addEvalEventListener();
+}
+
+function addEvalEventListener() {
+    eval.addEventListener('click', () => {
+        globalThis.secondNum = Number(display.textContent);
+        let operator = [...operators].filter(operator => operator.classList.contains('darker'))[0];
+        let result = operate(firstNum, secondNum, operator.textContent);
+        display.textContent = result;
+        operator.classList.remove('darker');
+    });
+}
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case '\u00F7':
+            return a/b;
+        case '\u00D7':
+            return  a*b;
+        case '\u2212':
+            return a-b;
+        case '\u002B':
+            return a+b;
+    }
+}
